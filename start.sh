e31f6958e68bf7d234c5edefaae55b2c10ac8b51
Mark Post <mark.post@live.nl>
2025-11-10 23:11:48 +0100

    refactor: simplify Docker script for service updates and cleanup

--- start.sh
+++ start.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-if [ "$1" == "onlyTrainInformation" ]; then
-  docker compose build --no-cache train-information
-  docker compose up -d train-information
-else
-  docker system prune -f
-  docker compose build --no-cache
-  docker compose up -d
-fi
+docker system prune -f
+docker compose build --no-cache
+docker compose up -d
--- update.sh
+++ update.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-# Stop train-information first (but leave others running)
-docker compose stop train-information
+# Pull updates only for redis and postgres
+docker compose pull redis postgres
 
-# Pull updates for everything except train-information
-docker compose pull $(docker compose config --services | grep -v '^train-information$')
-
-# Bring up the updated services (excluding train-information)
-docker compose up -d $(docker compose config --services | grep -v '^train-information$')
+# Bring up the updated services (only redis and postgres)
+docker compose up -d redis postgres
 
 # Prune the system
 docker system prune -f
 
-# Finally, bring back train-information (now that redis/postgres are updated)
-docker compose up -d train-information
