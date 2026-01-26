fb3297986b33dcfeddab7c54773b9c3b0005fe3f
Mark Post <mark.post@live.nl>
2026-01-26 21:38:24 +0100

    refactor: update User and LoginService tests to use builder pattern and improve readability

--- authentication-service/src/main/java/nl/markpost/authentication/config/JacksonConfig.java
+++ authentication-service/src/main/java/nl/markpost/authentication/config/JacksonConfig.java
@@ -1,7 +1,7 @@
 package nl.markpost.authentication.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import tools.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.openapitools.jackson.nullable.JsonNullableModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
